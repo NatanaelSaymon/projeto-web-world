ff1b2362941fd786c8742e8f91b2090b54602466
Natanael Saymon <nsaymonsouza@gmail.com>
2020-01-12 12:42:33 -0300

    Inserindo controles dos banners

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-$('.slider-principal').slick();
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 4000
+});
